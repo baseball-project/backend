849b6a85d3f8da7b0e0abb1c86e84095e3384c8b
seonghye0n <>
2023-11-15 17:47:10 +0900

    프로젝트 셋팅
    - Java 11 / SpringBoot 2.X
    - .gitignore 셋팅
    - application.yml 셋팅

---
+++ src/main/java/com/example/baseballprediction/BaseballPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.example.baseballprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballPredictionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseballPredictionApplication.class, args);
+    }
+
+}

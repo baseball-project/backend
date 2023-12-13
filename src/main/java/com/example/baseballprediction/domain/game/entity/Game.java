3c4e8ab97cc859a2c8c4937fca674ac0715c09c9
seonghye0n <>
2023-12-13 16:33:17 +0900

    feat : Team, Member, Game 엔티티 생성
    - Team : 야구팀
    - Member : 회원
    - Game : 야구 경기
    
    ref : #6

---
+++ src/main/java/com/example/baseballprediction/domain/game/entity/Game.java
@@ -0,0 +1,45 @@
+package com.example.baseballprediction.domain.game.entity;
+
+import com.example.baseballprediction.domain.team.entity.Team;
+import com.example.baseballprediction.global.constant.Status;
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "game_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "home_team_id")
+    @Column(nullable = false)
+    private Team homeTeam;
+
+    @OneToOne
+    @JoinColumn(name = "away_team_id")
+    @Column(nullable = false)
+    private Team awayTeam;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private Status status;
+
+    @Column(nullable = false)
+    private LocalDateTime startedAt;
+
+    @Column(name = "home_team_score", nullable = false)
+    @ColumnDefault("0")
+    private int homeTeamScore;
+
+    @Column(name = "away_team_score", nullable = false)
+    @ColumnDefault("0")
+    private int awayTeamScore;
+
+    @OneToOne
+    @JoinColumn(name = "win_team_id")
+    private Team winTeam;
+}

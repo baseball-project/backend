367c9f7d0b79b1b52e7280934d6761b45a267d09
seonghye0n <unknown>
2023-12-20 17:24:15 +0900

    feat : 좋아하는 팀 선택 API 구현
    - PUT /profile/team URL 매핑
    
    ref : #15

--- src/main/java/com/example/baseballprediction/domain/member/controller/MemberController.java
+++ src/main/java/com/example/baseballprediction/domain/member/controller/MemberController.java
@@ -1,15 +1,21 @@
 package com.example.baseballprediction.domain.member.controller;
 
-import com.example.baseballprediction.domain.member.dto.MemberRequest;
 import com.example.baseballprediction.domain.member.service.MemberService;
 import com.example.baseballprediction.global.security.auth.JwtTokenProvider;
+import com.example.baseballprediction.global.security.auth.MemberDetails;
+import com.example.baseballprediction.global.util.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+
+import static com.example.baseballprediction.domain.member.dto.MemberRequest.LikeTeamDTO;
+import static com.example.baseballprediction.domain.member.dto.MemberRequest.LoginDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +23,20 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody MemberRequest.LoginDTO loginDTO) {
+    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO) {
         Map<String, Object> response = memberService.login(loginDTO.getUsername(), loginDTO.getPassword());
 
         return ResponseEntity
                 .ok()
                 .header(JwtTokenProvider.HEADER, (String) response.get("token"))
                 .body("정상적으로 처리되었습니다.");
+    }
+
+    @PutMapping("/profile/team")
+    public ResponseEntity<ApiResponse<?>> likeTeamModify(@RequestBody LikeTeamDTO likeTeamDTO,
+                                                         @AuthenticationPrincipal MemberDetails memberDetails) {
+        memberService.modifyLikeTeam(memberDetails.getUsername(), likeTeamDTO.getTeamId());
+
+        return ResponseEntity.ok(ApiResponse.createSuccess());
     }
 }
--- src/main/java/com/example/baseballprediction/domain/member/service/MemberService.java
+++ src/main/java/com/example/baseballprediction/domain/member/service/MemberService.java
@@ -2,20 +2,26 @@ package com.example.baseballprediction.domain.member.service;
 
 import com.example.baseballprediction.domain.member.entity.Member;
 import com.example.baseballprediction.domain.member.repository.MemberRepository;
+import com.example.baseballprediction.domain.team.entity.Team;
+import com.example.baseballprediction.domain.team.repository.TeamRepository;
 import com.example.baseballprediction.global.security.auth.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final TeamRepository teamRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Transactional(readOnly = true)
     public Map<String, Object> login(String username, String password) {
         Member member = memberRepository.findByUsername(username).orElseThrow();
 
@@ -27,5 +33,12 @@ public class MemberService {
         response.put("token", JwtTokenProvider.createToken(member));
 
         return response;
+    }
+
+    public void modifyLikeTeam(String username, int teamId) {
+        Member member = memberRepository.findByUsername(username).orElseThrow();
+        Team likeTeam = teamRepository.findById(teamId).orElseThrow();
+
+        member.changeTeam(likeTeam);
     }
 }

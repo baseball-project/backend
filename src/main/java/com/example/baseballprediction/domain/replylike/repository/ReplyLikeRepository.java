0d3141bf8bb8ff15d5ba01e18069055e87f91321
seonghye0n <unknown>
2023-12-21 20:04:33 +0900

    feat : 댓글 좋아요 메서드 구현
    ref : #27

---
+++ src/main/java/com/example/baseballprediction/domain/replylike/repository/ReplyLikeRepository.java
@@ -0,0 +1,9 @@
+package com.example.baseballprediction.domain.replylike.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.baseballprediction.domain.replylike.entity.ReplyLike;
+
+public interface ReplyLikeRepository extends JpaRepository<ReplyLike, Long> {
+
+}
---
+++ src/main/java/com/example/baseballprediction/domain/replylike/service/ReplyLikeService.java
@@ -0,0 +1,34 @@
+package com.example.baseballprediction.domain.replylike.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.baseballprediction.domain.member.entity.Member;
+import com.example.baseballprediction.domain.member.repository.MemberRepository;
+import com.example.baseballprediction.domain.reply.entity.Reply;
+import com.example.baseballprediction.domain.reply.repository.ReplyRepository;
+import com.example.baseballprediction.domain.replylike.entity.ReplyLike;
+import com.example.baseballprediction.domain.replylike.repository.ReplyLikeRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ReplyLikeService {
+	private final ReplyLikeRepository replyLikeRepository;
+	private final ReplyRepository replyRepository;
+	private final MemberRepository memberRepository;
+
+	public void saveReplyLike(String username, Long replyId) {
+		Member member = memberRepository.findByUsername(username).orElseThrow();
+		Reply reply = replyRepository.findById(replyId).orElseThrow();
+
+		ReplyLike replyLike = ReplyLike.builder()
+			.member(member)
+			.reply(reply)
+			.build();
+
+		replyLikeRepository.save(replyLike);
+	}
+}

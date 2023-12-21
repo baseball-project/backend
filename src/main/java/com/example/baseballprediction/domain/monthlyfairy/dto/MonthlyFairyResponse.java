c16209d3670a55a6c95f962d9ea0e1e7d38daade
seonghye0n <>
2023-12-21 16:58:52 +0900

    feat : 월간 승리요정 조회 API 구현
    - MonthlyFairy 엔티티 생성
    - 월간 승리요정 조회 API 구현
    - 월간 승리요정 더미데이터 설정
    
    ref : #22

---
+++ src/main/java/com/example/baseballprediction/domain/monthlyfairy/dto/MonthlyFairyResponse.java
@@ -0,0 +1,41 @@
+package com.example.baseballprediction.domain.monthlyfairy.dto;
+
+import java.util.List;
+
+import com.example.baseballprediction.domain.monthlyfairy.entity.MonthlyFairy;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class MonthlyFairyResponse {
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class StatisticsDTO {
+		private List<FairyDTO> winMembers;
+		private List<FairyDTO> loseMembers;
+	}
+
+	@Getter
+	public static class FairyDTO {
+		private int rank;
+		private int voteRatio;
+
+		private String nickname;
+		private int winFairyCount;
+		private int loseFairyCount;
+		private String title;
+		private String teamLogoUrl;
+
+		public FairyDTO(MonthlyFairy monthlyFairy) {
+			this.rank = monthlyFairy.getRank();
+			this.voteRatio = monthlyFairy.getVoteRatio();
+			this.winFairyCount = monthlyFairy.getMember().getWinFairyCount();
+			this.loseFairyCount = monthlyFairy.getMember().getLoseFairyCount();
+			this.nickname = monthlyFairy.getMember().getNickname();
+			this.title = monthlyFairy.getMember().getComment();
+			this.teamLogoUrl = monthlyFairy.getMember().getTeam().getLogoUrl();
+		}
+	}
+}
---
+++ src/main/java/com/example/baseballprediction/domain/monthlyfairy/service/MonthlyFairyService.java
@@ -0,0 +1,66 @@
+package com.example.baseballprediction.domain.monthlyfairy.service;
+
+import static com.example.baseballprediction.domain.monthlyfairy.dto.MonthlyFairyResponse.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.baseballprediction.domain.member.entity.Member;
+import com.example.baseballprediction.domain.member.repository.MemberRepository;
+import com.example.baseballprediction.domain.monthlyfairy.entity.MonthlyFairy;
+import com.example.baseballprediction.domain.monthlyfairy.repository.MonthlyFairyRepository;
+import com.example.baseballprediction.global.constant.FairyType;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MonthlyFairyService {
+	private final MonthlyFairyRepository monthlyFairyRepository;
+	private final MemberRepository memberRepository;
+
+	public StatisticsDTO findStatistics() {
+		int currentMonth = Integer.parseInt(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMM")).toString());
+		List<MonthlyFairy> monthlyFairies = monthlyFairyRepository.findByMonth(currentMonth);
+
+		List<FairyDTO> winFairies = new ArrayList<>();
+		List<FairyDTO> loseFairies = new ArrayList<>();
+
+		for (MonthlyFairy monthlyFairy : monthlyFairies) {
+			findFairycount(monthlyFairy.getMember());
+			FairyDTO fairyDTO = new FairyDTO(monthlyFairy);
+
+			if (monthlyFairy.getType() == FairyType.WIN) {
+				winFairies.add(fairyDTO);
+				continue;
+			}
+
+			loseFairies.add(fairyDTO);
+		}
+		StatisticsDTO statistics = new StatisticsDTO(winFairies, loseFairies);
+
+		return statistics;
+	}
+
+	private void findFairycount(Member member) {
+		if (member == null) {
+			throw new RuntimeException("멤버를 찾을 수 없습니다.");
+		}
+
+		List<MonthlyFairy> monthlyFairies = monthlyFairyRepository.findByMember(member);
+
+		int winFairyCount = (int)monthlyFairies.stream()
+			.filter(monthlyFairy -> monthlyFairy.getType() == FairyType.WIN)
+			.count();
+
+		int loseFairyCount = (int)monthlyFairies.stream()
+			.filter(monthlyFairy -> monthlyFairy.getType() == FairyType.LOSE)
+			.count();
+
+		member.setFairyCount(winFairyCount, loseFairyCount);
+	}
+}

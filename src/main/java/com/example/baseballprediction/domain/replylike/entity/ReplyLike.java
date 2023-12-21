efee8511cf8fe5ca40e2ea27e75665803bf34e9e
seonghye0n <unknown>
2023-12-21 19:52:55 +0900

    feat : 댓글 좋아요 엔티티 생성
    - ReplyLike 엔티티 생성
    
    ref : #27

---
+++ src/main/java/com/example/baseballprediction/domain/replylike/entity/ReplyLike.java
@@ -0,0 +1,35 @@
+package com.example.baseballprediction.domain.replylike.entity;
+
+import com.example.baseballprediction.domain.BaseEntity;
+import com.example.baseballprediction.domain.member.entity.Member;
+import com.example.baseballprediction.domain.reply.entity.Reply;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReplyLike extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "reply_like")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "reply_id")
+	private Reply reply;
+}

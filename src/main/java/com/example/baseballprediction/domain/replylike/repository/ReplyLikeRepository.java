c46631d3d18f28477e8fe7cc18d48349cea36540
seonghye0n <unknown>
2023-12-21 20:07:31 +0900

    feat : 댓글 좋아요 건수 조회 메서드 구현
    ref : #27

--- src/main/java/com/example/baseballprediction/domain/replylike/repository/ReplyLikeRepository.java
+++ src/main/java/com/example/baseballprediction/domain/replylike/repository/ReplyLikeRepository.java
@@ -2,8 +2,9 @@ package com.example.baseballprediction.domain.replylike.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.baseballprediction.domain.reply.entity.Reply;
 import com.example.baseballprediction.domain.replylike.entity.ReplyLike;
 
 public interface ReplyLikeRepository extends JpaRepository<ReplyLike, Long> {
-
+	Long countByReply(Reply reply);
 }

18ea7ad8903108222a9906bdab3fb554aaa7dbdb
seonghye0n <>
2023-12-23 18:22:56 +0900

    feat : 월간 승리요정 댓글 삭제 API 구현
    - DELETE /statistics/replies/{replyId} 매핑
    - 본인이 작성한 댓글이 아닐 경우 예외처리
    
    ref : #34

--- src/main/java/com/example/baseballprediction/domain/monthlyfairy/controller/MonthlyFairyController.java
+++ src/main/java/com/example/baseballprediction/domain/monthlyfairy/controller/MonthlyFairyController.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,5 +63,13 @@ public class MonthlyFairyController {
 		replyService.addReply(ReplyType.FAIRY, memberDetails.getUsername(), replyDTO.getContent());
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.createSuccess());
+	}
+
+	@DeleteMapping("/replies/{replyId}")
+	public ResponseEntity<ApiResponse> replyRemove(@AuthenticationPrincipal MemberDetails memberDetails,
+		@PathVariable Long replyId) {
+		replyService.deleteReply(replyId, memberDetails.getUsername());
+
+		return ResponseEntity.ok(ApiResponse.successWithNoData());
 	}
 }

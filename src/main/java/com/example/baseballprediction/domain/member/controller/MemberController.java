b716dc549766ec12ecfcb27587653cc07a54f30b
seonghye0n <unknown>
2023-12-25 17:15:43 +0900

    feat : 프로필 팝업 조회 API 구현
    - GET /profiles/{memberId} 매핑
    - memberRepository에서 MonthlyFairy inner join하여 가져오도록 메서드 추가
    
    ref : #40

--- src/main/java/com/example/baseballprediction/domain/member/controller/MemberController.java
+++ src/main/java/com/example/baseballprediction/domain/member/controller/MemberController.java
@@ -1,50 +1,62 @@
 package com.example.baseballprediction.domain.member.controller;
 
-import com.example.baseballprediction.domain.member.service.MemberService;
-import com.example.baseballprediction.global.security.auth.JwtTokenProvider;
-import com.example.baseballprediction.global.security.auth.MemberDetails;
-import com.example.baseballprediction.global.util.ApiResponse;
-import lombok.RequiredArgsConstructor;
+import static com.example.baseballprediction.domain.member.dto.MemberRequest.*;
+
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import com.example.baseballprediction.domain.member.dto.ProfileProjection;
+import com.example.baseballprediction.domain.member.service.MemberService;
+import com.example.baseballprediction.global.security.auth.JwtTokenProvider;
+import com.example.baseballprediction.global.security.auth.MemberDetails;
+import com.example.baseballprediction.global.util.ApiResponse;
 
-import static com.example.baseballprediction.domain.member.dto.MemberRequest.*;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
-    private final MemberService memberService;
+	private final MemberService memberService;
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO) {
-        Map<String, Object> response = memberService.login(loginDTO.getUsername(), loginDTO.getPassword());
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO) {
+		Map<String, Object> response = memberService.login(loginDTO.getUsername(), loginDTO.getPassword());
 
-        return ResponseEntity
-                .ok()
-                .header(JwtTokenProvider.HEADER, (String) response.get("token"))
-                .body("정상적으로 처리되었습니다.");
-    }
+		return ResponseEntity
+			.ok()
+			.header(JwtTokenProvider.HEADER, (String)response.get("token"))
+			.body("정상적으로 처리되었습니다.");
+	}
 
-    @PutMapping("/profile/team")
-    public ResponseEntity<ApiResponse<?>> likeTeamModify(@RequestBody LikeTeamDTO likeTeamDTO,
-                                                         @AuthenticationPrincipal MemberDetails memberDetails) {
-        memberService.modifyLikeTeam(memberDetails.getUsername(), likeTeamDTO.getTeamId());
+	@PutMapping("/profile/team")
+	public ResponseEntity<ApiResponse<?>> likeTeamModify(@RequestBody LikeTeamDTO likeTeamDTO,
+		@AuthenticationPrincipal MemberDetails memberDetails) {
+		memberService.modifyLikeTeam(memberDetails.getUsername(), likeTeamDTO.getTeamId());
 
-        return ResponseEntity.ok(ApiResponse.createSuccess());
-    }
+		return ResponseEntity.ok(ApiResponse.createSuccess());
+	}
 
-    @PutMapping("/profile/details")
-    public ResponseEntity<ApiResponse<?>> detailsModify(@RequestBody DetailsDTO detailsDTO,
-                                                        @AuthenticationPrincipal MemberDetails memberDetails) {
+	@PutMapping("/profile/details")
+	public ResponseEntity<ApiResponse<?>> detailsModify(@RequestBody DetailsDTO detailsDTO,
+		@AuthenticationPrincipal MemberDetails memberDetails) {
 
-        memberService.modifyDetails(memberDetails.getUsername(), detailsDTO);
+		memberService.modifyDetails(memberDetails.getUsername(), detailsDTO);
 
-        return ResponseEntity.ok(ApiResponse.createSuccess());
-    }
+		return ResponseEntity.ok(ApiResponse.createSuccess());
+	}
+
+	@GetMapping("/profiles/{memberId}")
+	public ResponseEntity<ApiResponse<ProfileProjection>> profileList(@PathVariable Long memberId) {
+		ProfileProjection profile = memberService.findProfile(memberId);
+
+		return ResponseEntity.ok(ApiResponse.success(profile));
+	}
 }
--- src/main/java/com/example/baseballprediction/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/baseballprediction/domain/member/repository/MemberRepository.java
@@ -1,12 +1,26 @@
 package com.example.baseballprediction.domain.member.repository;
 
-import com.example.baseballprediction.domain.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByUsername(String username);
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-    Optional<Member> findByNickname(String nickname);
+import com.example.baseballprediction.domain.member.dto.ProfileProjection;
+import com.example.baseballprediction.domain.member.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	Optional<Member> findByUsername(String username);
+
+	Optional<Member> findByNickname(String nickname);
+
+	@Query(
+		"SELECT new com.example.baseballprediction.domain.member.dto.ProfileProjection(m.nickname, m.profileImageUrl, "
+			+ "sum(case when f.type = 'WIN' then 1 else 0 end) as winFairyCount, "
+			+ "sum(case when f.type = 'LOSE' then 1 else 0 end) as loseFairyCount) "
+			+ "FROM Member m "
+			+ "INNER JOIN MonthlyFairy f ON f.member = m "
+			+ "WHERE m.id = :memberId "
+			+ "GROUP BY m.nickname, m.profileImageUrl")
+	Optional<ProfileProjection> findProfile(@Param("memberId") Long memberId);
 }

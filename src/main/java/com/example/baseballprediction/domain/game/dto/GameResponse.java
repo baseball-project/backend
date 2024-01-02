8de4b1bb072d9ea44968b6e9ebdec4547ca6e13c
seonghye0n <>
2024-01-02 13:48:01 +0900

    refactor : 팀 로고 url 필드 삭제
    - 팀 로고는 클라이언트 쪽에서 처리하기로 하여 해당 필드 사용안함
    
    ref : #55

--- src/main/java/com/example/baseballprediction/domain/game/dto/GameResponse.java
+++ src/main/java/com/example/baseballprediction/domain/game/dto/GameResponse.java
@@ -1,34 +1,36 @@
 package com.example.baseballprediction.domain.game.dto;
 
 import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.LastModifiedDate;
+
 import com.example.baseballprediction.domain.game.entity.Game;
 import com.example.baseballprediction.domain.team.entity.Team;
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 
 public class GameResponse {
 
 	@Getter
 	@NoArgsConstructor
 	@AllArgsConstructor
-	public static class GameDtoDaily{
-		
+	public static class GameDtoDaily {
+
 		private Long gameId;
-		
+
 		private TeamDailyDTO homeTeam;
-		
+
 		private TeamDailyDTO awayTeam;
-		
+
 		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 		@LastModifiedDate
 		private LocalDateTime gameTime;
-		
+
 		private String status;
-		
+
 		public GameDtoDaily(Game game, Team homeTeam, Team awayTeam) {
 			this.gameId = game.getId();
 			this.homeTeam = new TeamDailyDTO(homeTeam);
@@ -36,22 +38,19 @@ public class GameResponse {
 			this.gameTime = game.getStartedAt();
 			this.status = game.getStatus().toString();
 		}
-		
-	}	
-		
-	@Getter	
-	public static class TeamDailyDTO{
-		
+
+	}
+
+	@Getter
+	public static class TeamDailyDTO {
+
 		private String teamName;
-		
-		private String teamLogoUrl;
-		
+
 		public TeamDailyDTO(Team team) {
 			this.teamName = team.getShortName();
-			this.teamLogoUrl = team.getLogoUrl();
 		}
-	
+
 	}
-	
+
 }
 	

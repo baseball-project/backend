d230384b19f88d5d5871de7f36ce8bf3e16b1a69
seonghye0n <unknown>
2024-01-02 13:48:01 +0900

    refactor : 팀 로고 url 필드 삭제
    - 팀 로고는 클라이언트 쪽에서 처리하기로 하여 해당 필드 사용안함
    
    ref : #55

--- src/main/java/com/example/baseballprediction/domain/team/dto/TeamResponse.java
+++ src/main/java/com/example/baseballprediction/domain/team/dto/TeamResponse.java
@@ -1,21 +1,20 @@
 package com.example.baseballprediction.domain.team.dto;
 
 import com.example.baseballprediction.domain.team.entity.Team;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class TeamResponse {
-    @Getter
-    @NoArgsConstructor
-    public static class TeamsDTO {
-        private int teamId;
-        private String teamName;
-        private String teamImageUrl;
+	@Getter
+	@NoArgsConstructor
+	public static class TeamsDTO {
+		private int teamId;
+		private String teamName;
 
-        public TeamsDTO(Team team) {
-            this.teamId = team.getId();
-            this.teamName = team.getName();
-            this.teamImageUrl = team.getLogoUrl();
-        }
-    }
+		public TeamsDTO(Team team) {
+			this.teamId = team.getId();
+			this.teamName = team.getName();
+		}
+	}
 }
--- src/main/java/com/example/baseballprediction/domain/team/entity/Team.java
+++ src/main/java/com/example/baseballprediction/domain/team/entity/Team.java
@@ -1,7 +1,12 @@
 package com.example.baseballprediction.domain.team.entity;
 
 import com.example.baseballprediction.domain.BaseEntity;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,18 +17,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Team extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "team_id")
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "team_id")
+	private int id;
 
-    @Column(unique = true, nullable = false, length = 30)
-    private String name;
+	@Column(unique = true, nullable = false, length = 30)
+	private String name;
 
-    @Column(unique = true, nullable = false, length = 10)
-    private String shortName;
-    @Column(length = 200)
-    private String logoUrl;
-    @Column(length = 16)
-    private String color;
+	@Column(unique = true, nullable = false, length = 10)
+	private String shortName;
+	@Column(length = 16)
+	private String color;
 }

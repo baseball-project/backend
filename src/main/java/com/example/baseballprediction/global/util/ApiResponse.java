96b3ab42e4aa7c5df907d37c839eccd67b97851e
seonghye0n <>
2024-01-09 18:38:50 +0900

    feat : createException 메서드 override
    - ErrorCode 타입을 변수로 받는 메서드 추가
    
    ref : #66

--- src/main/java/com/example/baseballprediction/global/util/ApiResponse.java
+++ src/main/java/com/example/baseballprediction/global/util/ApiResponse.java
@@ -9,6 +9,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 
+import com.example.baseballprediction.global.constant.ErrorCode;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,39 +20,43 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class ApiResponse<T> {
-    private static final String SUCCESS_MESSAGE = "정상적으로 처리되었습니다.";
-    private int code;
-    private String message;
-    private T data;
+	private static final String SUCCESS_MESSAGE = "정상적으로 처리되었습니다.";
+	private int code;
+	private String message;
+	private T data;
 
-    public static ApiResponse createSuccess() {
-        return new ApiResponse(HttpStatus.CREATED.value(), SUCCESS_MESSAGE, null);
-    }
+	public static ApiResponse createSuccess() {
+		return new ApiResponse(HttpStatus.CREATED.value(), SUCCESS_MESSAGE, null);
+	}
 
-    public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(HttpStatus.OK.value(), SUCCESS_MESSAGE, data);
-    }
+	public static <T> ApiResponse<T> success(T data) {
+		return new ApiResponse<>(HttpStatus.OK.value(), SUCCESS_MESSAGE, data);
+	}
 
-    public static ApiResponse successWithNoData() {
-        return new ApiResponse(HttpStatus.OK.value(), SUCCESS_MESSAGE, null);
-    }
+	public static ApiResponse successWithNoData() {
+		return new ApiResponse(HttpStatus.OK.value(), SUCCESS_MESSAGE, null);
+	}
 
-    public static ApiResponse<?> createValidationFail(BindingResult bindingResult) {
-        Map<String, String> errors = new HashMap<>();
+	public static ApiResponse<?> createValidationFail(BindingResult bindingResult) {
+		Map<String, String> errors = new HashMap<>();
 
-        List<ObjectError> allErrors = bindingResult.getAllErrors();
-        for (ObjectError error : allErrors) {
-            if (error instanceof FieldError) {
-                errors.put(((FieldError)error).getField(), error.getDefaultMessage());
-            } else {
-                errors.put(error.getObjectName(), error.getDefaultMessage());
-            }
-        }
+		List<ObjectError> allErrors = bindingResult.getAllErrors();
+		for (ObjectError error : allErrors) {
+			if (error instanceof FieldError) {
+				errors.put(((FieldError)error).getField(), error.getDefaultMessage());
+			} else {
+				errors.put(error.getObjectName(), error.getDefaultMessage());
+			}
+		}
 
-        return new ApiResponse<>(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.name(), errors);
-    }
+		return new ApiResponse<>(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.name(), errors);
+	}
 
-    public static ApiResponse<String> createException(int code, String message) {
-        return new ApiResponse<>(code, message, null);
-    }
+	public static ApiResponse<String> createException(int code, String message) {
+		return new ApiResponse<>(code, message, null);
+	}
+
+	public static ApiResponse<String> createException(ErrorCode errorCode) {
+		return new ApiResponse<>(errorCode.getStatusCode(), errorCode.getMessage(), null);
+	}
 }

bb0893cd2a72a0573bd28b3c5622f3801b1c17f3
HwiY <88710642+asapawinter@users.noreply.github.com>
2024-01-11 17:17:24 +0900

    오늘의 승부예측 댓글 조회 api
    * Feat: 오늘의 승부예측 댓글 작성 api
    
    - GameController에서 공통으로 만들어논 addReply연결
    
    ref: 68
    
    * Feat : 오늘의 승부예측 댓글 좋아요 기능 api
    
    -GameController에서 공통으로 만들어진 saveReplyLike 연결
    
    ref: 69
    
    * Feat : 오늘의 승부예측 조회 api
    
    -Controller에서 api명세서에 맞게 return 해주는 replyservice연결
    - replyType GAME, page를 리턴
    
    ref: 47
    
    * Feat: 오늘의 승부예측 댓글 조회 api
    -reply entity에 (name="reply")선언
    
    ref: 47
    
    * Feat : 오늘의 승부에측 댓글 조회api
    -JPQL로 QUERY를 생성
    -WHERE 조건절 1.replyType = GAME 2.createdAt = 오늘 날짜 yymmdd
    -createdAt desc 조건
    
    ref: 47
    
    * Feat: 오늘의 승부예측 댓글 조회 api
    -오늘의 승부예측 댓글 조회 repository연결 replyType,pageable를 담아줬습니다
    -Dto 2개가 존재
    -GameReplyDTO -> repository에서 모든 데이터를 담는 dto
    -GameListDTO -> api명세서에 맞게 가공한 dto
    
    ref: 47
    
    * Feat: 오늘의 승부예측 댓글 조회 api
    -enitity에 name="reply_like"선언
    
    ref:47
    
    * Feat: 오늘의 승부예측 댓글 조회 api
    -JPQL로 쿼리 생성
    - WHERE절 1. replyType = GAME 2.createdAt = 오늘 날짜
    
    ref: 47
    
    * Feat: 오늘의 승부에측 댓글 조회 api
    -JPQL로 만들어진 repository에서 select 절에 있는 모든 데이터를 한번에
    받아오는 dto
    
    ref:47
    
    * Feat: 오늘의 승부 예측 댓글 조회 api
    -GameReplyDTO에서 받아온 데이터를 api 명세서에 맞게 가공한  DTO
    
    ref: 47

---
+++ src/main/java/com/example/baseballprediction/domain/game/dto/GameReplyDTO.java
@@ -0,0 +1,44 @@
+package com.example.baseballprediction.domain.game.dto;
+
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.LastModifiedDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GameReplyDTO {
+	
+	private Long id;
+	
+	private Long count;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	@LastModifiedDate
+	private LocalDateTime createdAt;
+	
+	private String content;
+	
+	private String profileImageUrl;
+	
+	private String nickname;
+	
+	private String teamName;
+
+	
+	public GameReplyDTO(Long id,Long count,String content,LocalDateTime createdAt,String profileImageUrl,
+			String nickname,String teamName) {
+		this.id = id;
+		this.count = count;
+		this.content = content;
+		this.createdAt = createdAt;
+		this.profileImageUrl = profileImageUrl;
+		this.nickname = nickname;
+		this.teamName = teamName;
+	}
+
+	
+}

bb8a93d9d33b3c0ada33200d89b531e16ad8697e
seonghye0n <>
2024-01-13 18:59:42 +0900

    refactor : 월간 승리요정, 댓글  관련 비즈니스 로직 예외처리
    - 프로필 조회 -> 멤버 찾을 수 없을 때 예외처리
    - 댓글 작성 -> 멤버 찾을 수 없을 때 예외처리
    - 댓글 삭제 -> 멤버, 댓글 찾을 수 없을 때 예외처리
    - 댓글 삭제 -> 남이 작성한 댓글 삭제 시 예외처리
    
    ref : #78

--- src/main/java/com/example/baseballprediction/global/constant/ErrorCode.java
+++ src/main/java/com/example/baseballprediction/global/constant/ErrorCode.java
@@ -11,7 +11,10 @@ public enum ErrorCode {
 	ACCESS_DENIED(HttpStatus.FORBIDDEN, "로그인 후 이용해주세요."),
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
 
-	DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "중복되는 닉네임입니다.");
+	DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "중복되는 닉네임입니다."),
+	MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "사용자 정보를 찾을 수 없습니다."),
+	REPLY_NOT_FOUND(HttpStatus.BAD_REQUEST, "댓글 정보를 찾을 수 없습니다."),
+	REPLY_MEMBER_INVALID(HttpStatus.FORBIDDEN, "본인이 작성한 댓글만 수정/삭제가 가능합니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;

f3c0821030cea9f35c3a5c97135c419ef7e19c24
seonghye0n <>
2024-01-13 19:10:20 +0900

    refactor : 사용자 관련 비즈니스 로직 예외처리
    - 로그인 -> ID, 패스워드 불일치 예외처리
    - 좋아하는 팀 선택 -> 없는 팀 값 예외처리
    - 프로필 수정 -> 회원정보 못 찾을 때 예외처리
    - 프로필 수정 -> 닉네임 중복 예외처리
    - 프로필 조회 -> 회원정보 못 찾을 때 예외처리
    - 마이페이지 -> 회원정보 못 찾을 때 예외처리
    
    ref : #78

--- src/main/java/com/example/baseballprediction/global/constant/ErrorCode.java
+++ src/main/java/com/example/baseballprediction/global/constant/ErrorCode.java
@@ -11,10 +11,12 @@ public enum ErrorCode {
 	ACCESS_DENIED(HttpStatus.FORBIDDEN, "로그인 후 이용해주세요."),
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
 
-	DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "중복되는 닉네임입니다."),
+	MEMBER_NICKNAME_EXIST(HttpStatus.BAD_REQUEST, "중복되는 닉네임입니다."),
 	MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "사용자 정보를 찾을 수 없습니다."),
+	TEAM_NOT_FOUND(HttpStatus.BAD_REQUEST, "팀 정보를 찾을 수 없습니다."),
 	REPLY_NOT_FOUND(HttpStatus.BAD_REQUEST, "댓글 정보를 찾을 수 없습니다."),
-	REPLY_MEMBER_INVALID(HttpStatus.FORBIDDEN, "본인이 작성한 댓글만 수정/삭제가 가능합니다.");
+	REPLY_MEMBER_INVALID(HttpStatus.FORBIDDEN, "본인이 작성한 댓글만 수정/삭제가 가능합니다."),
+	LOGIN_PASSWORD_INVALID(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;

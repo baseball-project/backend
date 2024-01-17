5845f53c741f1191a0ef49a3d7fa928951d28d42
Hwiyoung Jeon <wjsgnldud@naver.com>
2024-01-17 11:00:04 +0900

    Feat: 오늘의 승부예측 투표 api -오늘의 승부예측 투표 , 투표 변경, 투표 취소 개발
    Ref:#73

--- src/main/java/com/example/baseballprediction/domain/game/controller/GameController.java
+++ src/main/java/com/example/baseballprediction/domain/game/controller/GameController.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.baseballprediction.domain.game.dto.GameReplyLikeProjection.GameListDTO;
 import com.example.baseballprediction.domain.game.dto.GameResponse.GameDtoDaily;
 import com.example.baseballprediction.domain.game.service.GameService;
+import com.example.baseballprediction.domain.gamevote.dto.GameVoteRequest.GameVoteRequestDTO;
+import com.example.baseballprediction.domain.gamevote.service.GameVoteService;
 import com.example.baseballprediction.domain.reply.dto.ReplyRequest;
 import com.example.baseballprediction.domain.reply.service.ReplyService;
 import com.example.baseballprediction.domain.replylike.service.ReplyLikeService;
@@ -29,10 +33,10 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/games")
 @RequiredArgsConstructor
 public class GameController {
-	
 	private final GameService gameService;
 	private final ReplyService replyService;
 	private final ReplyLikeService replyLikeService;
+	private final GameVoteService gameVoteService;
 	
 	//오늘의 승부예측 경기 list 조회
 	@GetMapping("")
@@ -77,5 +81,39 @@ public class GameController {
 
 		return ResponseEntity.ok(ApiResponse.successWithNoData());
 	}
+	
+	
+	//승부예측 투표
+	@PostMapping("/{gameId}/vote")
+	public ResponseEntity<ApiResponse> gameVoteAdd(@AuthenticationPrincipal MemberDetails memberDetails,
+			@RequestBody GameVoteRequestDTO gameVoteRequestDTO,
+			@PathVariable Long gameId) {
+		
+		gameVoteService.addGameVote(memberDetails.getUsername(),gameId,gameVoteRequestDTO);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.createSuccess());
+	}
+	
+	
+	//승부예측 투표 변경 
+	@PutMapping("/voteUpdate/{gameId}")
+	public ResponseEntity<ApiResponse> gameVoteModify(@AuthenticationPrincipal MemberDetails memberDetails,
+		@RequestBody GameVoteRequestDTO gameVoteRequestDTO,
+		@PathVariable Long gameId) {
+		gameVoteService.modifyGameVote(memberDetails.getUsername(),gameId,gameVoteRequestDTO);
+
+		return ResponseEntity.ok(ApiResponse.successWithNoData());
+	}
+			
+	
+	//승부예측 투표 취소 
+	@DeleteMapping("/voteDelete/{gameId}")
+	public ResponseEntity<ApiResponse> gameVoteRemove(@AuthenticationPrincipal MemberDetails memberDetails,
+		@PathVariable Long gameId) {
+		gameVoteService.removeGameVote(gameId, memberDetails.getUsername());
+
+		return ResponseEntity.ok(ApiResponse.successWithNoData());
+	}
+			
 
 }

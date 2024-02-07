151fdb2f740cdaa9dc45bf655cb8456b3082c128
Hwiyoung Jeon <wjsgnldud@naver.com>
2024-02-07 23:39:50 +0900

    Refactor: 오늘의 승부예측 조회 오류 수정
    -오늘의 승부예측 gameId, Query수정
    
    Ref: #101

--- src/main/java/com/example/baseballprediction/domain/gamevote/repository/GameVoteRepository.java
+++ src/main/java/com/example/baseballprediction/domain/gamevote/repository/GameVoteRepository.java
@@ -16,8 +16,8 @@ public interface GameVoteRepository extends JpaRepository<GameVote, Long> {
 	@Query(
 			value =
 			"SELECT "
-			+ "concat(round((( (select count(team_id) from game_vote gv2 where gv2.team_id =:homeTeamid and game_id =:gameId )  / count(game_id)) * 100 ))) as homeTeamVoteRatio ,"
-			+ " concat(round((( (select count(team_id) from game_vote gv2 where gv2.team_id =:awayTeamid and game_id =:gameId )  / count(game_id)) * 100 ))) as awayTeamVoteRatio "
+			+ " ifnull(concat(round((( (select count(team_id) from game_vote gv2 where gv2.team_id =:homeTeamid and game_id =:gameId )  / count(game_id)) * 100 ))),0) as homeTeamVoteRatio ,"
+			+ " ifnull(concat(round((( (select count(team_id) from game_vote gv2 where gv2.team_id =:awayTeamid and game_id =:gameId )  / count(game_id)) * 100 ))),0) as awayTeamVoteRatio "
 			+ " from game_vote gv "
 			+ " where game_id =:gameId", nativeQuery = true)
 	Optional<GameVoteRatioDTO> findVoteRatio(@Param("homeTeamid") int homeTeamid, @Param("awayTeamid") int awayTeamid, @Param("gameId") Long gameId);

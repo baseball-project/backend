0895f39597227cf75faec57ef125e8bf105286da
seonghye0n <unknown>
2024-02-26 12:33:08 +0900

    feat : 경기 상태, 스코어 업데이트 서비스 구현
    - KBO 홈페이지의 게임센터 페이지에서 당일 경기 상태, 스코어 업데이트하는 서비스 구현
    - Status 클래스 name 필드 추가 (KBO 게임센터 페이지 상, 경기예정(READY = cont, END = end, CANCEL = cancel)
    - 아직 경기 진행중인 상태가 게임센터 페이지에서 어떤 값을 갖는지 모르므로 추후에 업데이트 필요
    
    ref : #121

--- src/main/java/com/example/baseballprediction/domain/game/entity/Game.java
+++ src/main/java/com/example/baseballprediction/domain/game/entity/Game.java
@@ -1,57 +1,79 @@
 package com.example.baseballprediction.domain.game.entity;
 
-import com.example.baseballprediction.domain.team.entity.Team;
-import com.example.baseballprediction.global.constant.Status;
-import jakarta.persistence.*;
-import lombok.*;
+import java.time.LocalDateTime;
+
 import org.hibernate.annotations.ColumnDefault;
 
-import java.time.LocalDateTime;
+import com.example.baseballprediction.domain.team.entity.Team;
+import com.example.baseballprediction.global.constant.Status;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Game {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "game_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "game_id")
+	private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "home_team_id")
-    private Team homeTeam;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "home_team_id")
+	private Team homeTeam;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "away_team_id")
-    private Team awayTeam;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "away_team_id")
+	private Team awayTeam;
 
-    @Enumerated(EnumType.STRING)
-    @Column(length = 20)
-    private Status status;
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private Status status;
 
-    @Column(nullable = false)
-    private LocalDateTime startedAt;
+	@Column(nullable = false)
+	private LocalDateTime startedAt;
 
-    @Column(nullable = false)
-    @ColumnDefault("0")
-    private int homeTeamScore;
+	@Column(nullable = false)
+	@ColumnDefault("0")
+	private int homeTeamScore;
 
-    @Column(nullable = false)
-    @ColumnDefault("0")
-    private int awayTeamScore;
+	@Column(nullable = false)
+	@ColumnDefault("0")
+	private int awayTeamScore;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "win_team_id", unique = false)
-    private Team winTeam;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "win_team_id", unique = false)
+	private Team winTeam;
 
-    @Builder
-    public Game(Team homeTeam, Team awayTeam, Status status, LocalDateTime startedAt, int homeTeamScore, int awayTeamScore) {
-        this.homeTeam = homeTeam;
-        this.awayTeam = awayTeam;
-        this.status = status;
-        this.startedAt = startedAt;
-        this.homeTeamScore = homeTeamScore;
-        this.awayTeamScore = awayTeamScore;
-    }
+	@Builder
+	public Game(Team homeTeam, Team awayTeam, Status status, LocalDateTime startedAt, int homeTeamScore,
+		int awayTeamScore) {
+		this.homeTeam = homeTeam;
+		this.awayTeam = awayTeam;
+		this.status = status;
+		this.startedAt = startedAt;
+		this.homeTeamScore = homeTeamScore;
+		this.awayTeamScore = awayTeamScore;
+	}
+
+	public void updateByScrapeData(int homeTeamScore, int awayTeamScore, Status status) {
+		this.homeTeamScore = homeTeamScore;
+		this.awayTeamScore = awayTeamScore;
+		this.status = status;
+	}
 }

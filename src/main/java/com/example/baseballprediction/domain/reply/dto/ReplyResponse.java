f7587cc5a0bf6b9d75744089f85878fd36502bb8
seonghye0n <unknown>
2024-03-06 12:55:13 +0900

    refactor : 댓글 조회 응답 DTO 필드 수정
    - 기존 profile, reply 객체로 분리하던 것을 지우고 필드 나열하도록 수정
    
    ref : #138

--- src/main/java/com/example/baseballprediction/domain/reply/dto/ReplyResponse.java
+++ src/main/java/com/example/baseballprediction/domain/reply/dto/ReplyResponse.java
@@ -15,34 +15,25 @@ public class ReplyResponse {
 	@NoArgsConstructor
 	@AllArgsConstructor
 	public static class ReplyDTO {
-		private ProfileDTO profile;
-		private ReplyDetailsDTO reply;
-
-		public ReplyDTO(ReplyLikeProjection replyLikeProjection) {
-			this.profile = new ProfileDTO(replyLikeProjection.getProfileImageUrl(), replyLikeProjection.getNickname(),
-				replyLikeProjection.getTeamName());
-			this.reply = new ReplyDetailsDTO(replyLikeProjection.getId(), replyLikeProjection.getContent(),
-				replyLikeProjection.getCount(), replyLikeProjection.getCreatedAt());
-		}
-	}
-
-	@Getter
-	@AllArgsConstructor
-	public static class ProfileDTO {
 		private String profileImageUrl;
 		private String nickname;
 		private String teamName;
-	}
-
-	@Getter
-	@AllArgsConstructor
-	public static class ReplyDetailsDTO {
 		private Long replyId;
 		private String content;
-		private Long count;
+		private Long likeCount;
 
 		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 		@LastModifiedDate
 		private LocalDateTime createdAt;
+
+		public ReplyDTO(ReplyLikeProjection replyLikeProjection) {
+			this.profileImageUrl = replyLikeProjection.getProfileImageUrl();
+			this.nickname = replyLikeProjection.getNickname();
+			this.teamName = replyLikeProjection.getTeamName();
+			this.replyId = replyLikeProjection.getId();
+			this.content = replyLikeProjection.getContent();
+			this.likeCount = replyLikeProjection.getCount();
+			this.createdAt = replyLikeProjection.getCreatedAt();
+		}
 	}
 }

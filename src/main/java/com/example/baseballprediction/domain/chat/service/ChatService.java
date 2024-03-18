61c8d61deb478b59c4edfe87f91a0375038ed66a
Hwiyoung Jeon <wjsgnldud@naver.com>
2024-03-18 20:41:10 +0900

    Refactor: 채팅방 투표 완료 시에만 채팅방 입장 가능 & 게임이 종료 된 뒤 session 삭제 스케줄러 작업
    - StompHandler : 채팅방 투표 완료 후 입장 가능 로직 추가
    - GameVoteRepository : gameVote 테이블에서 사용자가 투표를 했는지 안
      했는지 체크
    - ChatService : game상태가 end일시 남아 있는 gameId에 대한 세션 삭제
    
    Ref: #161

--- src/main/java/com/example/baseballprediction/domain/chat/service/ChatService.java
+++ src/main/java/com/example/baseballprediction/domain/chat/service/ChatService.java
@@ -2,12 +2,17 @@ package com.example.baseballprediction.domain.chat.service;
 
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.example.baseballprediction.domain.game.repository.GameRepository;
 import com.example.baseballprediction.global.constant.ErrorCode;
+import com.example.baseballprediction.global.constant.Status;
 import com.example.baseballprediction.global.error.exception.NotFoundException;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +26,7 @@ public class ChatService {
     private Map<String, Set<String>> chatRooms = new ConcurrentHashMap<>();
     //클라이언트가 예기치 못하게게 끊겼을 경우 
     private ConcurrentHashMap<String, String> sessions = new ConcurrentHashMap<>();
+    private final GameRepository gameRepository;
     
     // 사용자가 채팅방에 입장할 때 호출됨
     public void addChatRoom(String sessionId, String gameId) {
@@ -46,4 +52,18 @@ public class ChatService {
     public void removeSession(String sessionId) {
         sessions.remove(sessionId);
     }
+    
+    @Scheduled(cron = "0 0/20 2 * * ?", zone = "Asia/Seoul")
+	public void removeSessionForEndedGames() {
+		List<Long> endedGameIds = gameRepository.findGameIdsByStatus(Status.END);
+		
+		if (endedGameIds == null || endedGameIds.isEmpty()) {
+		    return;
+		}
+		
+		endedGameIds.forEach(gameId -> {
+		    String gameIdString = String.valueOf(gameId);
+		    closeChatRoom(gameIdString);
+		});
+	}
 }

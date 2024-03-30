df31e4807d506e878b8e66dea4ebf90ab8c699ad
seonghye0n <>
2024-03-30 14:03:56 +0900

    refactor : 소셜 로그인 시, 엑세스 토큰 쿼리 파라미터로 전달
    - 현재 springboot oauth2-client 사용하고 있는데, 이렇게 되면 프론트엔드에게 토큰을 전달할 방법이 없음
    - 임시방편으로 쿼리 파라미터로 토큰 전달하도록 수정
    
    ref : #183

--- src/main/java/com/example/baseballprediction/global/security/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/example/baseballprediction/global/security/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,19 +1,15 @@
 package com.example.baseballprediction.global.security.oauth.handler;
 
-import static com.example.baseballprediction.global.security.oauth.dto.OAuthResponse.*;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.baseballprediction.global.security.MemberDetails;
 import com.example.baseballprediction.global.security.jwt.JwtTokenProvider;
-import com.example.baseballprediction.global.util.ApiResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -27,24 +23,27 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 		Authentication authentication) throws IOException, ServletException {
-		ObjectMapper objectMapper = new ObjectMapper();
+		// ObjectMapper objectMapper = new ObjectMapper();
 		MemberDetails memberDetails = (MemberDetails)authentication.getPrincipal();
 		String token = JwtTokenProvider.createToken(memberDetails.getMember());
 
-		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		// response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		// response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		//
+		// response.addHeader(JwtTokenProvider.HEADER, token);
 
-		response.addHeader(JwtTokenProvider.HEADER, token);
+		// boolean isNewMember = memberDetails.isNewMember();
+		// String teamName =
+		// 	memberDetails.getMember().getTeam() == null ? null : memberDetails.getMember().getTeam().getName();
+		// LoginDTO loginDTO = new LoginDTO(isNewMember, memberDetails.getProfileImageUrl(), memberDetails.getName(),
+		// 	teamName);
 
-		boolean isNewMember = memberDetails.isNewMember();
-		String teamName =
-			memberDetails.getMember().getTeam() == null ? null : memberDetails.getMember().getTeam().getName();
-		LoginDTO loginDTO = new LoginDTO(isNewMember, memberDetails.getProfileImageUrl(), memberDetails.getName(),
-			teamName);
+		String targetUrl = UriComponentsBuilder.fromUriString(request.getRequestURI())
+			.queryParam("accessToken", token)
+			.build()
+			.encode(StandardCharsets.UTF_8)
+			.toUriString();
 
-		ApiResponse<LoginDTO> apiResponse = ApiResponse.success(loginDTO);
-
-		String body = objectMapper.writeValueAsString(apiResponse);
-		response.getWriter().write(body);
+		response.sendRedirect(targetUrl);
 	}
 }

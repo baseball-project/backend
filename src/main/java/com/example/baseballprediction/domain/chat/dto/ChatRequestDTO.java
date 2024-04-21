9ea12854d67ddfcf27df873ec198938f4d2107e2
seonghye0n <seonghye0n@kakao.com>
2024-04-21 13:32:08 +0900

    feat : ChatRequestDTO 생성자 정의
    - NoArgsConstructor, AllArgsConstructor

--- src/main/java/com/example/baseballprediction/domain/chat/dto/ChatRequestDTO.java
+++ src/main/java/com/example/baseballprediction/domain/chat/dto/ChatRequestDTO.java
@@ -1,19 +1,23 @@
 package com.example.baseballprediction.domain.chat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class ChatRequestDTO {
-	
+
 	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
 	public static class ChatGiftRequestDTO {
 		private String recipientNickName;
 		private int token;
 		private String comment;
 	}
-	
+
 	@Getter
 	public static class ChatLeaveRequest {
-		 private Long gameId;
+		private Long gameId;
 
 	}
 }
